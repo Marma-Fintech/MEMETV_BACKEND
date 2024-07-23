0a21ba201ace4a6750e06e3cba41b8879364b3fc
PavieshThroughbit <paviesh@throughbit.com>
2024-07-23 10:25:11 +0530

    bug-fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -37,13 +37,24 @@ const login = async (req, res, next) => {
     const currentMonth = currentDate.getUTCMonth();
     const currentYear = currentDate.getUTCFullYear();
 
+    // Check if there's a refferedById and find the referring user
+    let referringUser = null;
+    if (refferedById) {
+      referringUser = await User.findOne({ refId: refferedById });
+
+      if (!referringUser) {
+        refferedById = ""; // Set refferedById to null if referring user not found
+        console.error('Referring user not found');
+      }
+    }
+
     if (!user) {
       // Create a new user with the generated refId and game cards
       user = new User({
         name,
         telegramId,
         refId,
-        refferedById,
+        refferedById, // This will be null if referring user was not found
         gameCard1,
         gameCard2,
         gameCard3,
@@ -65,76 +76,69 @@ const login = async (req, res, next) => {
       // Save the new user to the database
       await user.save();
 
-      // Check if there's a refferedById and find the referring user
-      if (refferedById) {
-        const referringUser = await User.findOne({ refId: refferedById });
-
+      if (referringUser) {
         // If referring user found, add the new user's userId to their yourReferenceIds
-        if (referringUser) {
-          referringUser.yourReferenceIds.push({ userId: user._id });
-          referringUser.totalRewards += 5000; // Add 5000 points for referral
+        referringUser.yourReferenceIds.push({ userId: user._id });
+        referringUser.totalRewards += 5000; // Add 5000 points for referral
 
-          // Add referral reward to the referring user's dailyRewards
-          const today = new Date();
-          today.setUTCHours(0, 0, 0, 0);
+        // Add referral reward to the referring user's dailyRewards
+        const today = new Date();
+        today.setUTCHours(0, 0, 0, 0);
 
-          const dailyReward = referringUser.dailyRewards.find(dr => {
-            const rewardDate = new Date(dr.createdAt);
-            rewardDate.setUTCHours(0, 0, 0, 0);
-            return rewardDate.getTime() === today.getTime();
-          });
+        const dailyReward = referringUser.dailyRewards.find(dr => {
+          const rewardDate = new Date(dr.createdAt);
+          rewardDate.setUTCHours(0, 0, 0, 0);
+          return rewardDate.getTime() === today.getTime();
+        });
 
-          if (dailyReward) {
-            dailyReward.totalRewards += 5000; // Update the existing daily reward for today
-          } else {
-            referringUser.dailyRewards.push({
-              userId: referringUser._id,
-              telegramId: referringUser.telegramId,
-              totalRewards: 5000,
-              createdAt: currentDate
-            });
-          }
-
-          // Add "2x" to the referring user's boosters array
-          referringUser.boosters.push("2x");
-
-          // Calculate additional milestone rewards if applicable
-          const numberOfReferrals = referringUser.yourReferenceIds.length;
-          const milestones = [
-            { referrals: 3, reward: 10000 },
-            { referrals: 5, reward: 16667 },
-            { referrals: 10, reward: 33333 },
-            { referrals: 20, reward: 66667 },
-            { referrals: 30, reward: 100000 },
-            { referrals: 40, reward: 133333 },
-            { referrals: 50, reward: 166667 },
-            { referrals: 60, reward: 200000 },
-            { referrals: 70, reward: 233333 },
-            { referrals: 80, reward: 266667 },
-            { referrals: 90, reward: 300000 },
-            { referrals: 100, reward: 333333 },
-          ];
-
-          // Add milestone rewards only once when milestones are reached
-          let totalMilestoneReward = 0;
-          for (const milestone of milestones) {
-            if (numberOfReferrals === milestone.referrals) {
-              totalMilestoneReward += milestone.reward;
-            }
-          }
-
-          referringUser.totalRewards += totalMilestoneReward;
-
-          if (totalMilestoneReward > 0) {
-            if (dailyReward) {
-              dailyReward.totalRewards += totalMilestoneReward; // Update the existing daily reward for today
-            } 
-          }
-
-          await referringUser.save();
+        if (dailyReward) {
+          dailyReward.totalRewards += 5000; // Update the existing daily reward for today
         } else {
-          console.error('Referring user not found');
+          referringUser.dailyRewards.push({
+            userId: referringUser._id,
+            telegramId: referringUser.telegramId,
+            totalRewards: 5000,
+            createdAt: currentDate
+          });
         }
+
+        // Add "2x" to the referring user's boosters array
+        referringUser.boosters.push("2x");
+
+        // Calculate additional milestone rewards if applicable
+        const numberOfReferrals = referringUser.yourReferenceIds.length;
+        const milestones = [
+          { referrals: 3, reward: 10000 },
+          { referrals: 5, reward: 16667 },
+          { referrals: 10, reward: 33333 },
+          { referrals: 20, reward: 66667 },
+          { referrals: 30, reward: 100000 },
+          { referrals: 40, reward: 133333 },
+          { referrals: 50, reward: 166667 },
+          { referrals: 60, reward: 200000 },
+          { referrals: 70, reward: 233333 },
+          { referrals: 80, reward: 266667 },
+          { referrals: 90, reward: 300000 },
+          { referrals: 100, reward: 333333 },
+        ];
+
+        // Add milestone rewards only once when milestones are reached
+        let totalMilestoneReward = 0;
+        for (const milestone of milestones) {
+          if (numberOfReferrals === milestone.referrals) {
+            totalMilestoneReward += milestone.reward;
+          }
+        }
+
+        referringUser.totalRewards += totalMilestoneReward;
+
+        if (totalMilestoneReward > 0) {
+          if (dailyReward) {
+            dailyReward.totalRewards += totalMilestoneReward; // Update the existing daily reward for today
+          }
+        }
+
+        await referringUser.save();
       }
     } else {
       // If the user already exists, add "levelUp" and "tap" to the boosters array if it's a new day
@@ -159,6 +163,7 @@ const login = async (req, res, next) => {
     next(err);
   }
 };
+
 
 
 
