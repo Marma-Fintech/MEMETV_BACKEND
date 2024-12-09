9c28f46d4b56b3226dfc769c25254feaede55dfd
pavieshkumar12 <dhanushpavi22@gmail.com>
2024-12-09 13:58:35 +0530

    FE-url-final

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (cluster.isMaster) {
           inline_keyboard: [
             [
               {
-                text: '#doNothingggggg',
+                text: '#doNothing',
                 web_app: {
                   url: `https://splendid-tapioca-cbf8e6.netlify.app/?start=${referredId}`
                 }
